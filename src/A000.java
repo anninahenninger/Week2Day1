54e9ead6d3d74c5e535fa681396cf93c98a45ec3
Annina Henninger <annina.henninger@aon.at>
2019-05-13 09:35:13 +0200

    Initial commit

---
+++ src/A000.java
@@ -0,0 +1,17 @@
+/**
+ * Create a class Person
+ *
+ *     As given through the live coding, with givenName and famillyName properties and corresponding get/set methods.
+ *
+ *     Create method getFullName() and call it on several Person objects. Improve your initial printing method to get
+ *     some nice table-like formatting.
+ *
+ *     INTERMEDIATE: Improve this implementation! Learn in Internet about the String method toUpperCase( )
+ *     and improve your getFullName, so it prints the person name in Format Givenname FAMILYNAME (like John DOE)
+ */
+public class A000 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/A001_MovieTicket.java
@@ -0,0 +1,24 @@
+/**
+ * Create a class MovieTicket for a cineplex-like movie theater.
+ *
+ *     This class holds following information:
+ *
+ *                 Price
+ *                 Date
+ *                 Time
+ *                 Movie
+ *                 cinema hall (1-10)
+ *
+ *
+ *     Create four  MovieTicket objects and store them in properties.
+ *
+ *     Create get and set methods accessing the movie ticket properties.
+ *
+ *     Now, calculate and store in a variable the total price of all created MovieObjects. Discuss with the group.
+ */
+public class A001_MovieTicket {
+
+    public static void main(String[] args){
+
+    }
+}
