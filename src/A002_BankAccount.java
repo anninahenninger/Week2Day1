92af57efe0120394bd2813f107bd165b724f20e6
Simon Blaha <s.blaha@live.at>
2019-05-13 13:44:11 +0200

    upload exercise a002

--- src/A002_BankAccount.java
+++ src/A002_BankAccount.java
@@ -24,5 +24,30 @@ public class A002_BankAccount {
 
     public static void main(String[] args){
 
+        BankAccount bankaccount1 = new BankAccount(2000);
+        BankAccount bankaccount2 = new BankAccount(1000000);
+        BankAccount bankaccount3 = new BankAccount(50000);
+
+        bankaccount1.addMoney(2000);
+        bankaccount1.withdrawMoney(50);
+        bankaccount1.printBalance();
+
+        bankaccount2.withdrawMoney(500000);
+        bankaccount2.printBalance();
+
+        bankaccount3.addMoney(15000);
+        bankaccount3.printBalance();
+
+        bankaccount1.withdrawMoney(2500);
+        bankaccount1.printBalance();
+        bankaccount1.withdrawMoney(1460);
+        bankaccount1.printBalance();
+
+        bankaccount2.addMoney(200);
+        bankaccount2.printBalance();
+        bankaccount2.withdrawMoney(150);
+        bankaccount2.printBalance();
+
+
     }
 }
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,13 +1,38 @@
 public class BankAccount {
+
     double balance;
 
-    public BankAccount(double balance){
+    public BankAccount(double balance) {
+
         this.balance = balance;
     }
-    public void addMoney(double money){
-        this.balance += money;
+
+    public void addMoney(double money) {
+        if(money == 0){
+            System.out.println("You can't add 0 to your account!");
+        } else if (money < 0) {
+            System.out.println("You can't add a negative number to your account!");
+        } else {
+            this.balance += money;
+            System.out.println("The amount of " + money + " has been added to your bank account. Your new balance is " + this.balance);
+        }
     }
+
+
     public void withdrawMoney(double money){
-        this.balance -= money;
+
+        if(this.balance < money) {
+            System.out.println("You can not withdrawl more money than your current balance wich is " + this.balance);
+            this.balance = 0;
+        }
+
+        else {
+            this.balance -= money;
+            System.out.println("The amount of " + money + " has been withdrawled from your bank account. Your new balance is " + this.balance);
+        }
+    }
+
+    public void printBalance(){
+        System.out.println(this.balance);
     }
 }
